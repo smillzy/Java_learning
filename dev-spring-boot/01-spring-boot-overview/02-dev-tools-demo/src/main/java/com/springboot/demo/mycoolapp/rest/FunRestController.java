5edfa57cdb8cc69a6b8f352dd7603820eec76567
Phoebe Lee <132736977+smillzy@users.noreply.github.com>
2023-11-28 22:34:49 +0800

    dev-tools

---
+++ dev-spring-boot/01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,23 @@
+package com.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5K!";
+    }
+}
