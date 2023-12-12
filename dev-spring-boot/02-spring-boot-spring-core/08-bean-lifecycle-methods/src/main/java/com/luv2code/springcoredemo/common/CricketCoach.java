79e4f525c08a9217662984b901068729d8d73982
Phoebe Lee <132736977+smillzy@users.noreply.github.com>
2023-12-13 05:50:49 +0800

    bean-lifecycle

---
+++ dev-spring-boot/02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,32 @@
+package com.luv2code.springcoredemo.common;
+
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define init
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff():" + getClass().getSimpleName());
+    }
+
+    //define destroy
+    @PreDestroy
+    public void doMyCleanupStuff(){
+        System.out.println("In doMyCleanupStuff():" + getClass().getSimpleName());
+    }
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice 15 min!??";
+    }
+}
