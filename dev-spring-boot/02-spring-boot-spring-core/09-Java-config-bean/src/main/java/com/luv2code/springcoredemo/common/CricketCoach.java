00fa0a90189fd1e16d9cbd1811bf81fbfa565d1f
Phoebe Lee <132736977+smillzy@users.noreply.github.com>
2023-12-13 22:28:02 +0800

    Java-config-bean

---
+++ dev-spring-boot/02-spring-boot-spring-core/09-Java-config-bean/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,20 @@
+package com.luv2code.springcoredemo.common;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice 15 min!??";
+    }
+}
